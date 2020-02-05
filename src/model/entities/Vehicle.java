2f686162aaf506c7fa10ff15949e902ec6007336
Luiz Leandrini <guilhermeleandrini@hotmail.com>
2020-02-05 15:29:55 -0300

    Aluguel carro

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,23 @@
+package model.entities;
+
+public class Vehicle {
+
+	private String model;
+	
+	public Vehicle() {
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Vehicle(String model) {
+		super();
+		this.model = model;
+	}
+	
+}
